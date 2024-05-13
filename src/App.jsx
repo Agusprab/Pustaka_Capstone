1d7c24783bfd493c37b7178e59247cf88385ab35
alwanjhs72@gmail.com <alwanjhs72@gmail.com>
2024-05-13 17:19:53 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-primary font-bold">Hello World!</h1>      
+    <h1 className="text-primary font-bold">Pustaka</h1>      
     </>
   )
 }
