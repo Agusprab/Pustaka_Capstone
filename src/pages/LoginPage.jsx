9b9c8dfef06edf995f086c1d2d71147de8968dc3
alwanjhs72@gmail.com <alwanjhs72@gmail.com>
2024-05-14 20:36:08 +0700

    Menambahkan asset logo di login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import LoginInput from "../components/LoginInput";
+import logo from "../asset/img/logo-pustaka.png";
+
 function LoginPage(){
     return(
         <div className="container">
             <div className="row mx-0">
-                <div className="col px-0">
-                    <img src="../asset/img/logo-pustaka.png" alt="" />
+                <div className="col px-0 bg-primary">
+                    <img src={logo} alt="Pustaka" />
                 </div>
                 <div className="col px-0"><LoginInput></LoginInput></div>
             </div>
