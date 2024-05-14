9539ea7ccf30d4fd39a2bcff44ff80f5c6cfffb1
alwanjhs72@gmail.com <alwanjhs72@gmail.com>
2024-05-14 20:00:18 +0700

    Menambahkan Text Website

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-primary font-bold">Pustaka Grup</h1>      
+    <h1 className="text-primary font-bold">Pustaka Website</h1>      
     </>
   )
 }
