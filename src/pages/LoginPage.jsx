2d2aee7cd4995bf0df56dcdb208ad94ca60e062b
Agusprab <agusbowo363@gmail.com>
2024-05-14 20:29:34 +0700

    perubahan pada login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,15 @@
+import LoginInput from "../components/LoginInput";
 function LoginPage(){
     return(
-        <h1>ini Login</h1>
+        <div className="container">
+            <div className="row mx-0">
+                <div className="col px-0">
+                    <img src="../asset/img/logo-pustaka.png" alt="" />
+                </div>
+                <div className="col px-0"><LoginInput></LoginInput></div>
+            </div>
+        </div>
+       
     )
 }
 
