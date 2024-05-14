bd2fa2ecb32f873402788a90253ab7f95443944d
alwanjhs72@gmail.com <alwanjhs72@gmail.com>
2024-05-14 21:27:08 +0700

    Menambahkan Register Input

---
+++ src/components/RegisterInput.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+function RegisterInput() {
+  return (
+    <form className="p-5">
+      <h1 className="font-poppins mt-5 text-center">Login</h1>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control shadow rounded-pill border-0"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control shadow rounded-pill border-0"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control shadow rounded-pill border-0"
+          id="exampleInputPassword1"
+        />
+      </div>
+
+      <div className="d-grid gap-2">
+        <button type="submit" className="btn primary-bg text-white">
+          Submit
+        </button>
+      </div>
+      <p className="mt-1">
+        Already have account ?{" "}
+        <Link to="/" className="link-opacity-100-hover">
+          {" "}
+          Login here {" "}
+        </Link>
+      </p>
+    </form>
+  );
+}
+
+export default RegisterInput;
