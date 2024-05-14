bfd06c2107d60f38e91d4382fd92c07ff8e9addc
Zainmuzadidzz <zmz250203@gmail.com>
2024-05-14 20:47:03 +0700

    test

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,17 @@
 import LoginInput from "../components/LoginInput";
-function LoginPage(){
-    return(
-        <div className="container">
-            <div className="row mx-0">
-                <div className="col px-0">
-                    <img src="../asset/img/logo-pustaka.png" alt="" />
-                </div>
-                <div className="col px-0"><LoginInput></LoginInput></div>
-            </div>
+function LoginPage() {
+  return (
+    <div className="container">
+      <div className="row mx-0">
+        <div className="col px-0">
+          <img src="../asset/img/logo-pustaka.png" alt="" />
         </div>
-       
-    )
+        <div className="col px-0">
+          <LoginInput></LoginInput>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
