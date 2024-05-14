33e387f0e8f37d8dc67eb431de1c98de57397827
Agusprab <agusbowo363@gmail.com>
2024-05-14 21:08:04 +0700

    perubahan login

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -1,21 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
 function LoginInput(){
   return(
-    <form className="p-3">
+    <form className="p-5">
         <h1 className="font-poppins mt-5 text-center">Login</h1>
     <div className="mb-3">
         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        <div id="emailHelp" className="form-text"> We'll never share your email with anyone else.</div>
+        <input type="email" className="form-control shadow border-0 rounded-pill" id="exampleInputEmail1 " aria-describedby="emailHelp"/>
+         
     </div>
     <div className="mb-3">
         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1"/>
+        <input type="password" className="form-control shadow border-0 rounded-pill" id="exampleInputPassword1"/>
     </div>
     <div className="mb-3 form-check">
         <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
         <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
     </div>
-    <button type="submit" className="btn  primary-bg">Submit</button>
+<div className="d-grid gap-2">
+    <button type="submit" className="btn btn-lg primary-bg text-white btn-submit">Submit</button>  
+</div>
+
 </form>
   )
 }
