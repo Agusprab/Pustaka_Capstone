aeed62bda633e732e146493476383ac7c2b2dd64
Agusprab <agusbowo363@gmail.com>
2024-05-15 21:05:00 +0700

    add dashboard admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import LandingPage from './pages/LandingPage';
+import AdminPage from './pages/AdminPage';
 function App() {
   return (
    <>
@@ -9,6 +10,7 @@ function App() {
         <Route path="/*" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/admin" element={<AdminPage/>}></Route>
       </Routes>
    </>
   )
---
+++ src/components/Dashboard/Aside.jsx
@@ -0,0 +1,90 @@
+import logo from "../../asset/img/logo-pustaka.png";
+function Aside() {
+    return(
+        <aside className="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+    <div className="sidenav-header">
+      <i className="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a className="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
+        <img src={logo} className="navbar-brand-img h-100" alt="main_logo" />
+        <span className="ms-1 font-weight-bold">Pustaka</span>
+      </a>
+    </div>
+    <hr className="horizontal dark mt-0" />
+    <div className="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" href="">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/tables.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Tables</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/billing.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-credit-card text-success text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Billing</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/virtual-reality.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-app text-info text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Virtual Reality</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/rtl.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-world-2 text-danger text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">RTL</span>
+          </a>
+        </li>
+        <li className="nav-item mt-3">
+          <h6 className="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/profile.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-single-02 text-dark text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Profile</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/sign-in.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Sign In</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="./pages/sign-up.html">
+            <div className="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i className="ni ni-collection text-info text-sm opacity-10"></i>
+            </div>
+            <span className="nav-link-text ms-1">Sign Up</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+   
+  </aside>
+    )
+
+}
+
+export default Aside;
