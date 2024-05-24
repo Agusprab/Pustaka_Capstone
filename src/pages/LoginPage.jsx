43c5fd30ba955e4737343a41f7ba2d3d8247d675
Agusprab <agusbowo363@gmail.com>
2024-05-24 20:41:47 +0700

    fix register UI

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -22,11 +22,13 @@ function LoginPage() {
   };
   return (
     <div className="container">
-      <div className="row mx-0 mt-lg-5 pt-lg-5 justify-content-center">
-        <div className="col-lg-5 d-none d-lg-block px-0 primary-bg text-center">
-          <Link to="/">
-            <img src={logo} alt="Pustaka" className="img img-fluid" />
-          </Link>
+      <div className="row mx-0 mt-lg-3 pt-lg-5 justify-content-center">
+        <div className="col-lg-5 d-none d-lg-block px-0 text-center">
+          <div className="row primary-bg">
+            <Link to="/" className="mb-5">
+              <img src={logo} alt="Pustaka" className="img img-fluid" />
+            </Link>
+          </div>
         </div>
         <div className="col-lg-5 col-12 px-0 ">
           <LoginInput onSubmit={handleLogin} />
