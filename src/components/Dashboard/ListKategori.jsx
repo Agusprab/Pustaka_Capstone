ec63caaacaccf4985c35161b35e65a43b51c0d24
Zainmuzadidzz <zmz250203@gmail.com>
2024-06-12 22:14:16 +0700

    menambah fitur search pada list user dan kategori

--- src/components/Dashboard/ListKategori.jsx
+++ src/components/Dashboard/ListKategori.jsx
@@ -1,32 +1,49 @@
-/* eslint-disable react/require-default-props */
-/* eslint-disable no-shadow */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable react/react-in-jsx-scope */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import {
-  getAllKategori,
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllKategori } from '../../features/kategoriSlice';
 
-} from '../../features/kategoriSlice';
-
-function ListKategori({ kategori, deleteKategori, isLoadingKategori }) {
+function ListKategori({ kategori, isLoadingKategori, deleteKategori }) {
   const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+
   useEffect(() => {
     dispatch(getAllKategori());
-  }, [kategori]);
+  }, [dispatch]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredKategori = kategori
+    ? kategori.filter((kat) =>
+        kat.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : [];
+
   return (
     <div className="row">
       <div className="col-12">
         <div className="card mb-4">
           <div className="card-header pb-0">
-            <h6>Authors table</h6>
-            <Link className="btn btn-primary" to="/admin/add-kategori">
-              <i className="fa fa-plus" aria-hidden="true" />
-              &nbsp; Tambah Kategori
-            </Link>
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h6>Authors table</h6>
+                <Link className="btn btn-primary" to="/admin/add-kategori">
+                  <i className="fa fa-plus" aria-hidden="true" />
+                  &nbsp; Tambah Kategori
+                </Link>
+              </div>
+              <input
+                type="text"
+                placeholder="Search by name"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="form-control"
+                style={{ display: 'inline', width: '200px', marginRight: '10px' }}
+              />
+            </div>
           </div>
 
           <div className="card-body px-0 pt-0 pb-2">
@@ -34,50 +51,29 @@ function ListKategori({ kategori, deleteKategori, isLoadingKategori }) {
               <table className="table align-items-center mb-0">
                 <thead>
                   <tr>
-                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                      ID Kategori
-                    </th>
-                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ">
-                      Nama Kategori
-                    </th>
-                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                      Employed
-                    </th>
+                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">ID Kategori</th>
+                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama Kategori</th>
+                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
                     <th className="text-secondary opacity-7" />
                   </tr>
                 </thead>
                 <tbody>
-                  {kategori && kategori.map((kategori) => (
-                    <tr key={kategori.uuid}>
+                  {filteredKategori.map((kat) => (
+                    <tr key={kat.uuid}>
                       <td className="align-middle text-center">
-                        <span className="text-secondary text-xs font-weight-bold">
-                          {kategori.id}
-                        </span>
+                        <span className="text-secondary text-xs font-weight-bold">{kat.id}</span>
                       </td>
                       <td className="align-middle text-center">
-                        <span className="text-secondary text-xs font-weight-bold">
-                          {kategori.name}
-                        </span>
+                        <span className="text-secondary text-xs font-weight-bold">{kat.name}</span>
                       </td>
                       <td className="align-middle text-center">
-                        <Link
-                          to={`/admin/edit-kategori/${kategori.uuid}`}
-                          className="text-secondary font-weight-bold text-xs"
-                        >
-                          Edit
-                        </Link>
+                        <Link to={`/admin/edit-kategori/${kat.uuid}`} className="text-secondary font-weight-bold text-xs">Edit</Link>
                         |
-                        <Link
-                          className="text-danger font-weight-bold text-xs"
-                          onClick={() => deleteKategori(kategori.uuid)}
-                        >
-                          Delete
-                        </Link>
+                        <Link className="text-danger font-weight-bold text-xs" onClick={() => deleteKategori(kat.uuid)}>Delete</Link>
                       </td>
                     </tr>
                   ))}
-                  {/* ini berguna ketika data belum di load */}
-                  {!kategori && (
+                  {!filteredKategori && (
                     <tr>
                       <td colSpan="4" className="text-center">
                         <div className="spinner-border" role="status">
@@ -86,8 +82,8 @@ function ListKategori({ kategori, deleteKategori, isLoadingKategori }) {
                       </td>
                     </tr>
 
-                  )}
-                  {/* ini berguna ketika ada perubahan di kategori */}
+                 )}
+                  {/* ini berguna ketika ada perubahan di  */}
                   {!isLoadingKategori && (
                   <tr>
                     <td colSpan="4" className="text-center">
@@ -98,7 +94,6 @@ function ListKategori({ kategori, deleteKategori, isLoadingKategori }) {
                   </tr>
 
                   )}
-
                 </tbody>
               </table>
             </div>
@@ -110,10 +105,9 @@ function ListKategori({ kategori, deleteKategori, isLoadingKategori }) {
 }
 
 ListKategori.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
   kategori: PropTypes.array,
-  deleteKategori: PropTypes.func,
-  isLoadingKategori: PropTypes.bool,
+  deleteKategori: PropTypes.func.isRequired,
+  isLoadingKategori: PropTypes.bool.isRequired,
 };
 
 export default ListKategori;
--- src/components/Dashboard/ListUser.jsx
+++ src/components/Dashboard/ListUser.jsx
@@ -4,31 +4,53 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable react/react-in-jsx-scope */
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { postedAt } from '../../utils/index';
 
-function ListUser({ users, deleteUser, isLoading }) {
+function ListUser({ users = [], deleteUser, isLoading }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <div className="row">
       <div className="col-12">
         <div className="card mb-4">
           <div className="card-header pb-0">
-            <h6>Authors table test</h6>
-            <Link className="btn btn-primary" to="/admin/add-user">
-              <i className="fa fa-plus" aria-hidden="true" />
-              &nbsp; Tambah User
-            </Link>
+            <div className="d-flex justify-content-between align-items-center">
+            <div>
+              <h6>Authors table test</h6>
+
+                <Link className="btn btn-primary" to="/admin/add-user">
+                  <i className="fa fa-plus" aria-hidden="true" />
+                  &nbsp; Tambah User
+                </Link>
+            </div>
+              <input
+                type="text"
+                placeholder="Search by name"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="form-control"
+                style={{ display: 'inline', width: '200px', marginRight: '10px' }}
+              />
+
+            </div>
           </div>
           <div className="card-body px-0 pt-0 pb-2">
             <div className="table-responsive p-0">
               <table className="table align-items-center mb-0">
                 <thead>
-                <tr>
-
-                  <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                  <tr>
+                    <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                       ID User
-                  </th>
+                    </th>
                     <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                       Nama
                     </th>
@@ -54,48 +76,51 @@ function ListUser({ users, deleteUser, isLoading }) {
                       Action
                     </th>
                     <th className="text-secondary opacity-7" />
-                </tr>
+                  </tr>
                 </thead>
                 <tbody>
-                {users && users.map((user) => (
-                  <tr key={user.uuid}>
-
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.id}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.name}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.email}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.jenisKelamin}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.alamat}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.noTlp}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{user.role}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <span className="text-secondary text-xs font-weight-bold">{postedAt(user.createdAt)}</span>
-                  </td>
-                  <td className="align-middle text-center">
-                    <Link to={`/admin/edit-user/${user.uuid}`} className="text-secondary font-weight-bold text-xs">
-                      Edit
-                    </Link>
-                    |
-                    <Link href="" className="text-danger font-weight-bold text-xs" onClick={() => deleteUser(user.uuid)}>
-                      Delete
-                    </Link>
-                  </td>
-                  </tr>
-                ))}
-                {!users && (
+                  {filteredUsers.map((user) => (
+                    <tr key={user.uuid}>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.id}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.name}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.email}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.jenisKelamin}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.alamat}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.noTlp}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{user.role}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">{postedAt(user.createdAt)}</span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <Link to={`/admin/edit-user/${user.uuid}`} className="text-secondary font-weight-bold text-xs">
+                          Edit
+                        </Link>
+                        |
+                        <Link
+                          href=""
+                          className="text-danger font-weight-bold text-xs"
+                          onClick={() => deleteUser(user.uuid)}
+                        >
+                          Delete
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                 {!users && (
                     <tr>
                       <td colSpan="9" className="text-center">
                         <div className="spinner-border" role="status">
@@ -104,7 +129,7 @@ function ListUser({ users, deleteUser, isLoading }) {
                       </td>
                     </tr>
 
-                )}
+                 )}
                   {/* ini berguna ketika ada perubahan di  */}
                   {!isLoading && (
                   <tr>
@@ -130,6 +155,7 @@ ListUser.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   users: PropTypes.array,
   deleteUser: PropTypes.func,
+  isLoading: PropTypes.bool,
 };
 
 export default ListUser;
